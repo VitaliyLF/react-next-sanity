a932e35af9a54edd07a73576feae1220bc052f87
Vitalii Stepnov <97784759+Vetosy@users.noreply.github.com>
2024-02-04 11:12:50 +0300

    delete 3 link

--- src/app/page.js
+++ src/app/page.js
@@ -41,18 +41,6 @@ export default function Home() {
 
       <div className={styles.grid}>
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className={styles.card}
           target="_blank"
